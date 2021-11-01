393683b19d2ae82fd293d993aa612061f6815742
Jude <jude@ai4gov.net>
2021-11-01 17:05:31 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [test, setTest] = useState("asd");
+
+  useEffect(() => {
+    // fetch("http://localhost:8001/testAPI")
+    //     .then(res => res.text())
+    //     .then(res => setTest(res))
+    //     .catch(err => console.log(err));
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. { test }
         </p>
         <a
           className="App-link"
